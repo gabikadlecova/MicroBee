5a02ce36c06bccb21743cb882d41d6e5131169b8
Gabi Suchopar <gabi.suchoparova@gmail.com>
2018-09-05 23:49:51 +0200

    Layout modifications

---
+++ MicroBee/MicroBee/ViewModels/Converters/CountZeroConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace MicroBee.ViewModels.Converters
+{
+	class CountZeroConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			if (targetType != typeof(int))
+			{
+				throw new InvalidOperationException();
+			}
+
+			int intVal = (int)value;
+			return intVal == 0;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new InvalidOperationException("Converter is only one-way.");
+		}
+	}
+}
